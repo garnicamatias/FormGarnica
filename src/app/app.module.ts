dada9db7c3bc066146a5d539388ed4aea8b3bf86
garnicamatias <garnicamatias02@gmail.com>
2023-02-02 22:18:15 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SigninFormComponent } from './components/signin-form/signin-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SigninFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/signin-form/signin-form.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signin-form',
+  templateUrl: './signin-form.component.html',
+  styleUrls: ['./signin-form.component.css']
+})
+export class SigninFormComponent {
+
+  signinForm : FormGroup;
+  
+  constructor(){
+
+    let regexEmail : string = '^([a-z0-9]+(?:[._-][a-z0-9]+)*)@([a-z0-9]+(?:[.-][a-z0-9]+)*\.[a-z]{2,})$';
+    let regexPassword : string = '^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$';
+    let controls: any = {
+      name : new FormControl('', [Validators.required]),
+      surname : new FormControl('',[Validators.required]),
+      username : new FormControl('',[Validators.required, Validators.minLength(5), Validators.maxLength(15)]),
+      email : new FormControl('',[Validators.required, Validators.pattern(regexEmail)]),
+      password : new FormControl('', [Validators.required, Validators.pattern(regexPassword)]),
+      usertype : new FormControl('', [Validators.required])
+    }
+
+    this.signinForm = new FormGroup(controls);
+  }
+
+  getClass(input: string): string{
+  
+    if (this.signinForm.controls[input].valid) {
+      return 'validInput'
+    }
+
+    switch (input) {
+      case 'name':
+      case 'surname':
+
+      if (this.signinForm.controls[input].touched && this.signinForm.controls[input].errors?.['required'] ) 
+      return 'invalidInput';
+      break;
+    
+      case 'username':
+      
+      if (this.signinForm.controls['username'].touched && this.signinForm.controls['username'].errors?.['required'] || (this.signinForm.controls['username'].errors?.['minlength'] || this.signinForm.controls['username'].errors?.['maxlength'])) 
+      return 'invalidInput';
+      break;
+
+      case 'email':
+      if ((this.signinForm.controls['email'].touched && this.signinForm.controls['email'].errors?.['required']) || this.signinForm.controls['email'].errors?.['pattern']) 
+
+      return 'invalidInput'
+      break;
+
+      case 'password' :
+      if ((this.signinForm.controls['password'].touched && this.signinForm.controls['password'].errors?.['required']) || this.signinForm.controls['password'].errors?.['pattern']) 
+      
+      return 'invalidInput'
+      break;
+
+    }
+
+    return 'form-control'
+  }
+
+  signin(){
+    console.log(this.signinForm)
+  }
+}
