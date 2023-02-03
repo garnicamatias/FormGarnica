2ae0a99c450ffd31af6509347f9e1e4931811fe4
garnicamatias <garnicamatias02@gmail.com>
2023-02-02 23:12:55 -0300

    regex password fixed

--- src/app/components/signin-form/signin-form.component.ts
+++ src/app/components/signin-form/signin-form.component.ts
@@ -13,7 +13,7 @@ export class SigninFormComponent {
   constructor(){
 
     let regexEmail : string = '^([a-z0-9]+(?:[._-][a-z0-9]+)*)@([a-z0-9]+(?:[.-][a-z0-9]+)*\.[a-z]{2,})$';
-    let regexPassword : string = '^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$';
+    let regexPassword : string = '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$';
     let controls: any = {
       name : new FormControl('', [Validators.required]),
       surname : new FormControl('',[Validators.required]),
